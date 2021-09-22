64fa70a2ea5662170d2d5207f465c63b82d5a8b6
alireza kargar <imanesche@gmail.com>
2021-09-23 03:29:37 +0330

    add toast to add component for customer

--- src/app/modules/customer/components/Add.tsx
+++ src/app/modules/customer/components/Add.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import * as API from "../../../../API/public";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default class Add extends React.Component<any, any> {
     constructor(props: any) {
@@ -31,6 +33,10 @@ export default class Add extends React.Component<any, any> {
         API.add_customer(this.state.formValues)
             .then((res: any) => {
                 console.log(res.data)
+                toast("کاربر شما با موفقیت اضافه شد", {
+                    type: "info",
+                    theme: "dark"
+                })
             })
 
     }
@@ -39,6 +45,9 @@ export default class Add extends React.Component<any, any> {
         const {address} = this.state
         return (
             <section>
+                <ToastContainer
+                    position="bottom-center"
+                />
                 <div className="row" dir="rtl">
                     <div className="col-md-6">
                         <div className="mb-10">
